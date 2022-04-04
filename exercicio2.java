ad2239b693a82cb635b27b1756d9d4b683a9d04e
Sabrina Godoy <sabrinapenteadogodoy2003@gmail.com>
2022-04-04 15:29:34 -0300

    laço de repetição

---
+++ exercicio2.java
@@ -0,0 +1,32 @@
+package exercicioRepetição;
+
+import java.util.Scanner;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		int x,i,par=0,impar=0;
+		Scanner ler=new Scanner(System.in);
+	
+		for(i=1;i<=10;i++) {
+			System.out.println("escreva um numero: ");
+		    x=ler.nextInt();
+		
+		  if(x%2==0) {
+			  par++;
+			 
+		  }
+		  else {
+			impar++;
+		  }
+		 
+		  }
+		System.out.println("temos "+par+" numeros pares e "+impar+" impares.");
+		System.out.println("fim do programa:)");
+	}
+}
+
+		  
+	
+
+
